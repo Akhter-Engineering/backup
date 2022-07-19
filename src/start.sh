6b520fc468d8ffb4e1d617fdde8c3d7c4f1d2e40
Azamat <a.b@akhter.studio>
2022-07-19 20:19:27 +0600

    add prefix

--- src/start.sh
+++ src/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "${CRON_EXPRESSION} python ${ROOT}/main.py >> /dev/stdout" > crontab.txt
+echo "${BACKUP_CRON_EXPRESSION} python ${ROOT}/main.py >> /dev/stdout" > crontab.txt
 crontab crontab.txt
 /usr/sbin/crond -f -l 8

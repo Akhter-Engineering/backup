51262a9a61745b9bc47ed56bae0926735717a0b1
Azamat <a.b@akhter.studio>
2022-08-26 16:50:14 +0600

    refactor using context

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!python
 
+from asyncio.log import logger
 import os
 import logging
 import boto
@@ -12,44 +13,58 @@ from slack import WebClient
 from slack.errors import SlackApiError
 
 
-BACKUP_APP_NAME = os.getenv('BACKUP_APP_NAME')
-BACKUP_AWS_REGION = os.getenv('BACKUP_AWS_REGION')
-BACKUP_AWS_ACCESS_KEY_ID = os.getenv('BACKUP_AWS_ACCESS_KEY_ID')
-BACKUP_AWS_SECRET_ACCESS_KEY = os.getenv('BACKUP_AWS_SECRET_ACCESS_KEY')
-BACKUP_AWS_BUCKET_NAME = os.getenv('BACKUP_AWS_BUCKET_NAME')
-
-BACKUP_SLACK_API_TOKEN = os.getenv('BACKUP_SLACK_API_TOKEN')
-BACKUP_SLACK_CHANNEL = os.getenv('BACKUP_SLACK_CHANNEL')
-
-
 logging.basicConfig(level=logging.INFO)
 
 
-def get_output_filename():
-    return '%s_%s.sql' % (BACKUP_APP_NAME, datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%SZ'))
+class Context:
+    
+    APP_NAME = 'app_name'
+    AWS_REGION = 'aws_region'
+    AWS_ACCESS_KEY_ID = 'aws_access_key_id'
+    AWS_SECRET_ACCESS_KEY = 'aws_secret_access_key'
+    AWS_BUCKET_NAME = 'aws_bucket_name'
+    SLACK_CHANNEL = 'slack_channel'
+    SLACK_API_TOKEN = 'slack_api_token'
+
+    @staticmethod
+    def get_initial_context():
+        context = {
+            Context.APP_NAME: os.getenv('BACKUP_APP_NAME'),
+            Context.AWS_REGION: os.getenv('BACKUP_AWS_REGION'),
+            Context.AWS_ACCESS_KEY_ID: os.getenv('BACKUP_AWS_ACCESS_KEY_ID'),
+            Context.AWS_SECRET_ACCESS_KEY: os.getenv('BACKUP_AWS_SECRET_ACCESS_KEY'),
+            Context.AWS_BUCKET_NAME: os.getenv('BACKUP_AWS_BUCKET_NAME'),
+            Context.SLACK_CHANNEL: os.getenv('BACKUP_SLACK_CHANNEL'),
+            Context.SLACK_API_TOKEN: os.getenv('BACKUP_SLACK_API_TOKEN'),
+        }
+        return context
 
 
-def create_temp_backup_sql(temp_path):
-    os.system('PGPASSWORD=$BACKUP_POSTGRES_PASSWORD pg_dump -h $BACKUP_POSTGRES_HOST -p $BACKUP_POSTGRES_PORT -U $BACKUP_POSTGRES_USER $BACKUP_POSTGRES_DB > %s' % temp_path)
+def get_output_filename(prefix_name, context):
+    return '%s_%s.sql' % (prefix_name, datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%SZ'))
 
 
-def upload_temp_backup_sql_into_s3(source, output):
+def create_temp_backup_sql(path, context):
+    os.system('PGPASSWORD=$BACKUP_POSTGRES_PASSWORD pg_dump -h $BACKUP_POSTGRES_HOST -p $BACKUP_POSTGRES_PORT -U $BACKUP_POSTGRES_USER $BACKUP_POSTGRES_DB > %s' % path)
+
+
+def upload_temp_backup_sql_into_s3(source, output, context):
     conn = boto.s3.connect_to_region(
-        BACKUP_AWS_REGION,
-        aws_access_key_id=BACKUP_AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=BACKUP_AWS_SECRET_ACCESS_KEY,
+        context.get(Context.AWS_REGION),
+        aws_access_key_id=context.get(Context.AWS_ACCESS_KEY_ID),
+        aws_secret_access_key=context.get(Context.AWS_SECRET_ACCESS_KEY),
     )
-    bucket = conn.get_bucket(BACKUP_AWS_BUCKET_NAME)
+    bucket = conn.get_bucket(context.get(Context.AWS_BUCKET_NAME))
     k = Key(bucket)
     k.key = output
     k.set_contents_from_filename(source)
 
 
-def notify_backup_result_to_slack(channel, text):
-    slack_client = WebClient(token=BACKUP_SLACK_API_TOKEN)
+def notify_backup_result_to_slack(text, context):
+    slack_client = WebClient(token=context.get(Context.SLACK_API_TOKEN))
     try:
         response = slack_client.chat_postMessage(
-            channel=channel,
+            channel=context.get(Context.SLACK_CHANNEL),
             text=text
         )
     except SlackApiError as e:
@@ -58,15 +73,26 @@ def notify_backup_result_to_slack(channel, text):
 
 
 def main():
+    context = Context.get_initial_context()
+    
     try:
-        temp_path = 'backup.sql'
-        output_filename= get_output_filename()
-        create_temp_backup_sql(temp_path)
-        upload_temp_backup_sql_into_s3(temp_path, output_filename)
-        notify_backup_result_to_slack(BACKUP_SLACK_CHANNEL,
-            ":green_heart: Created a postgresql backup `%s` for application `%s` in AWS bucket `%s`" % (output_filename, BACKUP_APP_NAME, BACKUP_AWS_BUCKET_NAME))
+        output_filename = get_output_filename(context.get(Context.APP_NAME), context)
+        temp_path = '/tmp/%s' % output_filename
+        create_temp_backup_sql(temp_path, context)
+        upload_temp_backup_sql_into_s3(
+            temp_path,
+            output_filename,
+            context,
+        )
+        notify_backup_result_to_slack(
+            ":green_heart: Created a postgresql backup `%s` for application `%s` in AWS bucket `%s`" % (output_filename, context.get(Context.APP_NAME), context.get(Context.AWS_BUCKET_NAME)),
+            context,
+        )
     except Exception as e:
-        notify_backup_result_to_slack(BACKUP_SLACK_CHANNEL, ":broken_heart: Error: `%s` ```%s```" % (BACKUP_APP_NAME, e))
+        notify_backup_result_to_slack(
+            ":broken_heart: Error: `%s` ```%s```" % (context.get(Context.APP_NAME), e),
+            context,
+        )
         raise e
 
 
